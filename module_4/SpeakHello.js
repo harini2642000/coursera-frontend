d99a14f6e8770825cb6140450a851cc842316959
Harini G <32165816+harini2642000@users.noreply.github.com>
2020-10-04 23:06:33 +0530

    module4

--- module_4/SpeakHello.js
+++ module_4/SpeakHello.js
@@ -1,8 +1,10 @@
 (function(window) {
+	var helloSpeaker={};
 	var speakWord = "Hello";
 	var helloSpeaker = function (name) {
 		console.log(speakWord + " " + name);
 	}
 
 	window.helloSpeaker = helloSpeaker;
+
 })(window);
